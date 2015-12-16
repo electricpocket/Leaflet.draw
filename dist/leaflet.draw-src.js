a4406e5e4b779056b433ec7ccb2df4feee012ab2
Steve Bennett <steve@electricpocket.com>
2015-12-16 13:58:36 +0000

    erwh

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1849,7 +1849,7 @@ L.GeometryUtil = L.extend(L.GeometryUtil || {}, {
         var y = Math.sin(dLon) * Math.cos(lat2);
         var x = Math.cos(lat1)*Math.sin(lat2) - Math.sin(lat1)*Math.cos(lat2)*Math.cos(dLon);
         var brng = this._toDeg(Math.atan2(y, x));
-        return 360 - ((brng + 360) % 360);
+        return ((brng + 360) % 360);
     },
 
    /**
